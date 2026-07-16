f6caccdfed7221db8431ca90c34cb7777b878cb6
Steven J <tonystevenj@gmail.com>
2026-07-15 23:11:13 -0700

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/guitar-practice/',
+  base: '/',
   build: { outDir: 'docs' },
 })
